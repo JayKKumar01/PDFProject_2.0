dd773b5e11d5a941b277dd7043a7c18b524fce23
Jay Kumar <jaytechbc@gmail.com>
2024-02-15 01:57:31 +0530

    word to pdf conversion fixed

--- src/main/java/pdfproject/Launcher.java
+++ src/main/java/pdfproject/Launcher.java
@@ -33,36 +33,47 @@ public class Launcher {
         }
 
         // Iterate through the list of DataModel objects
-        for (DataModel data : list) {
-            // Create an instance of PDFProject with paths from DataModel
-            PDFProject pdfProject = new PDFProject(data.getPath1(), data.getPath2());
-
-            // Set page range for the first file if specified in DataModel
-            if (data.isRange1()) {
-                pdfProject.setPageRangeForFile1(data.getStartPage1(), data.getEndPage1());
-            }
-
-            // Set page range for the second file if specified in DataModel
-            if (data.isRange2()) {
-                pdfProject.setPageRangeForFile2(data.getStartPage2(), data.getEndPage2());
-            }
-
-            // Create the output folder based on the configuration
-            File outputFolder = new File(Config.OUTPUT_IMAGES_PATH);
-            outputFolder = new File(outputFolder, data.getOutputFolder());
-            pdfProject.setOutputPath(outputFolder.getAbsolutePath());
-
-            // Check if the output folder exists, otherwise skip the current iteration
-            if (!outputFolder.exists()) {
-                System.out.println("Couldn't create: " + outputFolder.getAbsolutePath());
+        for (int i=0; i<list.size(); i++) {
+            DataModel data = list.get(i);
+            if (data == null){
+                System.out.println("Skipped Row: "+(i+2));
                 continue;
             }
 
-            // Perform the PDF comparison
-            if (pdfProject.compare()) {
-                System.out.println(data.getOutputFolder() + ": Comparison Done!");
-            } else {
-                System.out.println(data.getOutputFolder() + ": Something went wrong!");
+            try {
+                // Create an instance of PDFProject with paths from DataModel
+                PDFProject pdfProject = new PDFProject(data.getPath1(), data.getPath2());
+
+                // Set page range for the first file if specified in DataModel
+                if (data.isRange1()) {
+                    pdfProject.setPageRangeForFile1(data.getStartPage1(), data.getEndPage1());
+                }
+
+                // Set page range for the second file if specified in DataModel
+                if (data.isRange2()) {
+                    pdfProject.setPageRangeForFile2(data.getStartPage2(), data.getEndPage2());
+                }
+
+                // Create the output folder based on the configuration
+                File outputFolder = new File(Config.OUTPUT_IMAGES_PATH);
+                outputFolder = new File(outputFolder, data.getOutputFolder());
+                pdfProject.setOutputPath(outputFolder.getAbsolutePath());
+
+                // Check if the output folder exists, otherwise skip the current iteration
+                if (!outputFolder.exists()) {
+                    System.out.println("Couldn't create: " + outputFolder.getAbsolutePath());
+                    continue;
+                }
+
+                // Perform the PDF comparison
+                if (pdfProject.compare()) {
+                    System.out.println(data.getOutputFolder() + ": Comparison Done!");
+                } else {
+                    System.out.println(data.getOutputFolder() + ": Something went wrong!");
+                }
+            } catch (Exception e) {
+                System.out.println("Error: "+e.getMessage());
+                System.out.println("Skipped Row: "+(i+2));
             }
         }
 
--- src/main/java/pdfproject/utils/WordToPdfConverter.java
+++ src/main/java/pdfproject/utils/WordToPdfConverter.java
@@ -7,29 +7,27 @@ import pdfproject.Config;
 import pdfproject.Launcher;
 
 import java.io.*;
+import java.util.concurrent.*;
 
 public class WordToPdfConverter {
     public static final String TEMP_DIR = System.getProperty("java.io.tmpdir");
+    private static final int MAX_RETRIES = 2;
+    private static final int RETRY_SLEEP_MILLIS = 1000;
+    private static final int TIMEOUT_SECONDS = 20;
+    private static IConverter converter = null;
 
     public static File toPDF(String docPath) {
         File inputWord = new File(docPath);
+
         if (!inputWord.exists()) {
             return null;
         }
-
-        if (!Launcher.isInProgress) {
-            if (Config.UserGuide.CHECK_IF_WORD_IS_RUNNING) {
-                if (isWordRunning()) {
-                    System.out.println("Make sure to close MS Word!");
-                    return null;
-                }
-            } else {
-                if (isWordRunning()) {
-                    System.out.println("To save your MS Word work data, Can't Proceed as the MS Word is still Running");
-                    return null;
-                }
+        if (!Launcher.isInProgress){
+            if(!isAllowed()){
+                return null;
             }
         }
+
         Launcher.isInProgress = true;
 
         System.out.println("Converting... : " + inputWord.getName());
@@ -37,27 +35,92 @@ public class WordToPdfConverter {
         DocumentType type = DocumentType.MS_WORD;
         File outputPdf = new File(TEMP_DIR + "/" + System.currentTimeMillis() + ".pdf");
 
-        try (InputStream inputStream = new FileInputStream(inputWord);
-             OutputStream outputStream = new FileOutputStream(outputPdf)) {
 
-            IConverter converter = LocalConverter.builder().build();
-            converter.convert(inputStream).as(type).to(outputStream).as(DocumentType.PDF).execute();
-            converter.shutDown();
+        int remainingAttempts = 2;
 
-            Launcher.tempFiles.add(outputPdf);
+        while (remainingAttempts > 0) {
+            CompletableFuture<File> conversionFuture = CompletableFuture.supplyAsync(() -> {
+                File attemptOutputPdf = new File(TEMP_DIR + "/" + System.currentTimeMillis() + ".pdf");
 
-            System.out.println("Converted!");
+                for (int attempt = 1; attempt <= MAX_RETRIES; attempt++) {
+                    try (InputStream inputStream = new FileInputStream(inputWord);
+                         OutputStream outputStream = new FileOutputStream(attemptOutputPdf)) {
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+                        converter = LocalConverter.builder().build();
+                        converter.convert(inputStream).as(type).to(outputStream).as(DocumentType.PDF).execute();
+                        Launcher.tempFiles.add(attemptOutputPdf);
+
+                        System.out.println("Converted!");
+                        return attemptOutputPdf; // Successful conversion, exit the loop
+
+                    } catch (Exception e) {
+                        handleConversionException(inputWord, attempt, e);
+
+                        if (attempt < MAX_RETRIES) {
+                            sleepBeforeRetry();
+                        }
+                    } finally {
+                        closeConverter(converter);
+                    }
+                }
+
+                return null; // If the conversion fails after all retries
+            });
+
+            try {
+                outputPdf = conversionFuture.get(TIMEOUT_SECONDS, TimeUnit.SECONDS);
+                break;
+            } catch (InterruptedException | ExecutionException | TimeoutException e) {
+                System.out.println("Conversion process timed out: " + e.getMessage());
+                remainingAttempts--;
+            }finally {
+                closeConverter(converter);
+            }
         }
 
         return outputPdf;
     }
 
+    private static boolean isAllowed() {
+        if (Config.UserGuide.CHECK_IF_WORD_IS_RUNNING) {
+            if (isWordRunning()) {
+                System.out.println("Make sure to close MS Word!");
+                return false;
+            }
+        } else {
+            if (isWordRunning()) {
+                System.out.println("To save your MS Word work data, Can't Proceed as MS Word is still Running");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void handleConversionException(File inputWord, int attempt, Exception e) {
+        System.out.println("Error: " + inputWord.getName());
+        System.out.println("Re-attempt: " + attempt);
+    }
+
+    private static void sleepBeforeRetry() {
+        try {
+            TimeUnit.MILLISECONDS.sleep(RETRY_SLEEP_MILLIS);
+        } catch (InterruptedException ignored) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private static void closeConverter(IConverter converter) {
+        try {
+            if (converter != null) {
+                converter.shutDown();
+            }
+        } catch (Exception ignored) {
+            // Log or handle the exception as needed
+        }
+    }
+
     private static boolean isWordRunning() {
         try {
-            // Run 'tasklist' command and check if "WINWORD.EXE" is in the output
             Process process = Runtime.getRuntime().exec("tasklist");
             try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
 
